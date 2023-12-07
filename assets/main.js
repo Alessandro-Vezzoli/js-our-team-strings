87be791bcb82c39679fefd67aa0229d0ae6b3dd1
alessandro vezzoli <alessandro.vezzoli@vp360.eu>
2023-12-07 11:39:43 +0100

    add bonus 2 finish work

--- assets/main.js
+++ assets/main.js
@@ -20,45 +20,63 @@ Inserire un form per l’aggiunta di un elemento alla lista.
 
 //MILESTONE 1
 
-// creare un array di stringhe conn i nome del membri dei team
-var userName = ["Luca", "Matteo", "Asia", "Alessandro"];
-
 //stampare le informazioni su delle card
-for (var i = 0; i < userName.length; i++) {
-  console.log(userName[i]);
+function updateList(userName) {
+  //pulisci lista
+  document.getElementById("card-container").innerHTML = "";
+  userName.forEach((user) => {
+    console.log(user);
 
-  const div = document.createElement("div");
-  div.className = "card m-3";
-  div.style.width = "18rem";
-  console.log(div);
+    const div = document.createElement("div");
+    div.className = "card m-3";
+    div.style.width = "18rem";
+    console.log(div);
 
-  const img = document.createElement("img");
-  img.src = "./img/user.png";
-  img.className = "card-img-top";
-  img.alt = "user";
-  console.log(img);
+    const img = document.createElement("img");
+    img.src = "./img/user.png";
+    img.className = "card-img-top";
+    img.alt = "user";
+    console.log(img);
 
-  const secondDiv = document.createElement("div");
-  secondDiv.className = "card-body";
-  console.log(secondDiv);
+    const secondDiv = document.createElement("div");
+    secondDiv.className = "card-body";
+    console.log(secondDiv);
 
-  const titleUser = document.createElement("h5");
-  titleUser.className = "card-title text-center";
-  titleUser.textContent = userName[i];
-  console.log(titleUser);
+    const titleUser = document.createElement("h5");
+    titleUser.className = "card-title text-center";
+    titleUser.textContent = user;
+    console.log(titleUser);
 
-  const button = document.createElement("a");
-  button.className = "btn btn-primary";
-  button.textContent = "Scopri di più";
-  button.onclick = function () {
-    div.className = "d-none";
-  };
+    const button = document.createElement("a");
+    button.className = "btn btn-danger";
+    button.textContent = "Rimuovi Utente";
+    button.onclick = function () {
+      div.className = "d-none";
+    };
 
-  console.log(button);
+    console.log(button);
 
-  secondDiv.appendChild(titleUser);
-  div.appendChild(img);
-  div.appendChild(secondDiv);
-  div.appendChild(button);
-  document.getElementById("card-container").appendChild(div);
+    secondDiv.appendChild(titleUser);
+    div.appendChild(img);
+    div.appendChild(secondDiv);
+    div.appendChild(button);
+    document.getElementById("card-container").appendChild(div);
+  });
 }
+// creare un array di stringhe conn i nome del membri dei team
+let userName = ["Luca", "Matteo", "Asia", "Alessandro"];
+updateList(userName);
+
+//creare form per aggiungere utenti
+
+document.querySelector("#addNameUser").addEventListener("click", function () {
+  const addName = document.getElementById("addUser").value;
+
+  userName.push(addName);
+  console.log("adduser", addName);
+  updateList(userName);
+});
+
+document.querySelector("#formAdd").addEventListener("click", function () {
+  document.getElementById("formAddUser").classList.remove("d-none");
+});
